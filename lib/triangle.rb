70570eeaf5129b279768ba546c97c203d222733c
Kevin Britt <kbritt510@gmail.com>
2020-08-13 04:41:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,6 @@
 class Triangle
-  
+
+attr_accessor :,   
   
   
   
@@ -16,3 +17,39 @@ class TriangleError < StandardError
   
   
 end
+
+
+class Person
+  attr_accessor :partner, :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def get_married(person)
+    self.partner = person
+    if person.class != Person
+      begin
+      raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+      end
+    else
+      person.partner = self
+    end
+  end
+  
+  class PartnerError < StandardError
+    def message
+      "you must give the get_married method an"
+  end
+
+end
+
+beyonce = Person.new("Beyonce")
+beyonce.get_married("Jay-Z")
+puts beyonce.name
+
+
+
+
