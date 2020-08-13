4a105ba5a70fddc7a7655060c6381a8027550331
Kevin Britt <kbritt510@gmail.com>
2020-08-13 04:49:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,45 +10,20 @@ end
 def kind
   if @side_one <= 0 || @side_two <= 0 || @side_three <= 0
     raise TriangleError
+    rescue TriangleError => error
+      puts error.message
+    end
+  
+  
 end
 
 class TriangleError < StandardError
-  
+  def message
+    "Pythagoras is angry that you don't understand triangles."
+  end
   
   
 end
-
-
-class Person
-  attr_accessor :partner, :name
-
-  def initialize(name)
-    @name = name
-  end
-
-  def get_married(person)
-    self.partner = person
-    if person.class != Person
-      begin
-      raise PartnerError
-      rescue PartnerError => error
-        puts error.message
-      end
-    else
-      person.partner = self
-    end
-  end
-  
-  class PartnerError < StandardError
-    def message
-      "you must give the get_married method an"
-  end
-
-end
-
-beyonce = Person.new("Beyonce")
-beyonce.get_married("Jay-Z")
-puts beyonce.name
 
 
 
